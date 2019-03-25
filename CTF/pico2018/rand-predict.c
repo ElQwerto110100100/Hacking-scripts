91273ac66b8db9f4751a2986f5d6364ec9445f94
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-03-25 18:14:06 +1100

    test this on linux should have better results

--- CTF/pico2018/rand-predict.c
+++ CTF/pico2018/rand-predict.c
@@ -17,45 +17,42 @@
 
 long cash = 0;
 long wins = 0;
-long spin = 0;
 
-int is_digit(char c) {
-    return '0' <= c && c <= '9';
-}
+const char *win_msgs[NUM_WIN_MSGS] = {
+  "Wow.. Nice One!",
+  "You chose correct!",
+  "Winner!",
+  "Wow, you won!",
+  "Alright, now you're cooking!",
+  "Darn.. Here you go",
+  "Darn, you got it right.",
+  "You.. win.. this round...",
+  "Congrats!",
+  "You're not cheating are you?",
+};
 
-long get_long() {
-    printf("> ");
-    uint64_t l = 0;
-    char c = 0;
-    while(!is_digit(c))
-      c = getchar();
-    while(is_digit(c)) {
-      if(l >= LONG_MAX) {
-	l = LONG_MAX;
-	break;
-      }
-      l *= 10;
-      l += c - '0';
-      c = getchar();
-    }
-    while(c != '\n')
-      c = getchar();
-    return l;
-}
+const char *lose_msgs1[NUM_LOSE_MSGS] = {
+  "WRONG",
+  "Nice try..",
+  "YOU LOSE",
+  "Not this time..",
+  "Better luck next time..."
+};
 
-long get_rand() {
-  long seed;
-  seed = get_long();
-  seed = seed % 5000;
-  if (seed < 0) seed = seed * -1;
+const char *lose_msgs2[NUM_LOSE_MSGS] = {
+  "Just give up!",
+  "It's over for you.",
+  "Stop wasting your time.",
+  "You're never gonna win",
+  "If you keep it up, maybe you'll get the flag in 100000000000 years"
+};
+
+int main(int argc, char *argv[]) {
+  long seed = 2476;
   srand(seed);
-  return seed;
-}
-
-main () {
-  get_rand();
-  for (int i = 0; i< 10; i++) {
-    spin = (rand() % ROULETTE_SIZE)+1;
-    printf("Spin #'%d': '%lu' \n", i,spin);
+  for (int i = 0; i< 20; i++) {
+    long spin = (rand() % ROULETTE_SIZE)+1;
+    printf("\n Spin #'%d': '%lu' \n", i,spin);
   }
+  return 1;
 }
