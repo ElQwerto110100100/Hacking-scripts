735daa609840974c20e66a00d2ae7d21800983ac
ElQwerto110100100 <joshyboy111@hotmail.com.au>
2019-03-26 16:40:29 +1100

    changes

--- CTF/pico2018/Roulette/test.c
+++ CTF/pico2018/Roulette/test.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <limits.h>
 
+long cash = 0;
+
 int is_digit(char c) {
     return '0' <= c && c <= '9';
 }
@@ -17,11 +19,18 @@ long get_long() {
       c = getchar();
     while(is_digit(c)) {
       if(l >= LONG_MAX) {
-	l = LONG_MAX;
-	break;
+	       l = LONG_MAX;
+         printf("l >= LONG_MAX uint64_t l: %lu \n",l);
+	        break;
       }
+      printf("uint64_t l: %lu \n",l);
+      //printf("uint64_t l signed version: %ls \n",l);
       l *= 10;
+      printf("uint64_t l *= 10: %lu \n",l);
+      //printf("uint64_t l signed version: %ls \n",l);
       l += c - '0';
+      printf("uint64_t l: += %c = %lu \n",c, l);
+      //printf("uint64_t l signed version: %ls \n",l);
       c = getchar();
     }
     while(c != '\n')
@@ -30,14 +39,23 @@ long get_long() {
 }
 
 int main(int argc, char *argv[]) {
-  long cash = ULONG_MAX;
-  uint64_t l = LONG_MAX;
-  printf("%lu \n",ULONG_MAX);
-  printf("%lu \n",LONG_MAX );
-  printf("%lu \n",cash );
-  printf("%lu \n",l );
+  //cash = ULONG_MAX;
+  //uint64_t l = LONG_MAX;
+  //bit too diffcult to print out max value of uint64_t
+  //printf("%lu \n",ULONG_MAX); //outputs 4294967295
+  printf("%lu \n",LONG_MAX ); //outputs 2147483647
+  //printf("%li \n",cash ); //outputs -1 with long signed notation
+  //printf("%lu \n",cash ); //outputs 294967295 with long unsigned notation
+  //printf("%lu \n",l );
 
-  
+  //printf("\nsize of uint64_t: %d \n",sizeof(uint64_t));
+  //printf("size of long: %d \n",sizeof(long));
 
+  long bet = get_long();
+  printf("bet unsigned version: %lu \n",bet);
+  printf("bet signed version: %ls \n",bet);
+  cash -= bet;
+  printf("cash unsigned version %lu \n", cash);
+  printf("cash signed version %ls \n", cash);
   return 0;
 }
